6ab3a81f28cb72ca713b286db73dcb618867e359
Xiaoda Wang <sig.xiaoda@gmail.com>
2019-03-07 21:04:58 -0800

    fix runtime error

--- number_of_islands/number_of_islands.rb
+++ number_of_islands/number_of_islands.rb
@@ -8,7 +8,7 @@ def num_islands(grid)
   count = 0
   grid.each_with_index do |row, i|
     row.each_with_index do |col, j|
-      next if col == 0
+      next if col == '0'
       count += 1
       erase_island(grid, [i, j])
     end 
@@ -18,21 +18,22 @@ end
 
 def erase_island(grid, coord)
   y, x = coord
-  return if grid[y][x] == 0
-  grid[y][x] = 0
-  erase_island(grid, [y - 1, x]) unless y == 0
-  erase_island(grid, [y + 1, x]) unless y == grid.count - 1
-  erase_island(grid, [y, x - 1]) unless x == 0
-  erase_island(grid, [y, x + 1]) unless x == grid[0].count - 1
+  return if x < 0 || y < 0 ||  x >= grid[0].count || \
+    y >= grid.count || grid[y][x] == '0'
+  grid[y][x] = '0'
+  erase_island(grid, [y - 1, x]) 
+  erase_island(grid, [y + 1, x])
+  erase_island(grid, [y, x - 1])
+  erase_island(grid, [y, x + 1])
 end
 # rspec
 describe 'num_islands' do
   it 'returns 1 island' do
     grid = [
-      [1, 1, 1, 1, 0],
-      [1, 1, 0, 1, 0],
-      [1, 1, 0, 0, 0],
-      [0, 0, 0, 0, 0]
+      ['1', '1', '1', '1', '0'],
+      ['1', '1', '0', '1', '0'],
+      ['1', '1', '0', '0', '0'],
+      ['0', '0', '0', '0', '0']
     ]
 
     expect(num_islands(grid)).to eq(1)
@@ -40,12 +41,18 @@ describe 'num_islands' do
 
   it 'returns 3 islands' do
     grid = [
-      [1, 1, 0, 0, 0],
-      [1, 1, 0, 0, 0],
-      [0, 0, 1, 0, 0],
-      [0, 0, 0, 1, 1]
+      ['1', '1', '0', '0', '0'],
+      ['1', '1', '0', '0', '0'],
+      ['0', '0', '1', '0', '0'],
+      ['0', '0', '0', '1', '1']
     ]
 
     expect(num_islands(grid)).to eq(3)
+  end
+
+  it 'returns 1 island' do
+    grid = [["1","1","1"],["1","0","1"],["1","1","1"]]
+
+    expect(num_islands(grid)).to eq(1)
   end
 end
